94147a559dfe8d6a3a589737a7a1f4495b26fd83
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-09-27 19:53:15 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && position_taken?
+  if index.between?(0,8) && !position_taken?(board, index)
         true
   else
         false
